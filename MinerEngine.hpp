501f173666c8a916e21a5f2e8611c54c69b8b09c
lauret_s <sebastien.lauret@epitech.eus>
2017-05-02 12:57:49 -0400

    begin engine worker and session class

---
+++ MinerEngine.hpp
@@ -0,0 +1,14 @@
+#ifndef MINERENGINE_HPP
+# define MINERENGINE_HPP
+
+class MinerEngine {
+public:
+  MinerEngine(Option &opt);
+  ~MinerEngine();
+
+
+private:
+
+};
+
+#endif //!MINERENGINE_HPP
---
+++ MinerWorker.hpp
@@ -0,0 +1,11 @@
+#ifndef MINERWORKER_HPP
+# define MINERWORKER_HPP
+
+class MinerWorker {
+public:
+  MinerWorker()
+private:
+
+};
+
+#endif //!MINERWORKER_HPP
