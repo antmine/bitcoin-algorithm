fa2e123b4e050636486ec4eb875ac97b3b737b36
lauret_s <sebastien.lauret@epitech.eus>
2017-05-02 12:58:46 -0400

    begin session class

---
+++ Session.hpp
@@ -0,0 +1,14 @@
+#ifndef OPTIONS_HPP
+# define OPTIONS_HPP
+
+class Options {
+public:
+  std::string _clientId;
+  std::string _siteId;
+  std::string _delay;
+  std::string _forceUIThread;
+  std::string _autoStart;
+  std::string _workerTimeout;
+};
+
+#endif //!SESSION_HPP
